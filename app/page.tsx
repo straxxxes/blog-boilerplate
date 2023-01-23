9a46502755ed201407053c0396afaaa3e17c076e
strax <straxes.riley@gmail.com>
2023-01-23 18:22:35 +0200

    completed navbar style & responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <>
       {/* this is the initial root / landing page the visitor will see */}
-      <main></main>
+      <main>
+        <h1 className="text-3xl">a random heading that has many words</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+          possimus corrupti quis fuga dicta repellat autem sint, aperiam cum,
+          dolorem dolorum officia quisquam ipsam consequuntur, adipisci odio
+          unde suscipit perferendis!
+        </p>
+      </main>
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,21 +11,14 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="flex items-center flex-wrap bg-green-400 p-3 ">
+      <nav className="flex items-center content-center align-middle flex-wrap p-3 ">
         <Link href="/" className="inline-flex items-center p-2 mr-4 ">
-          <svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            className="fill-current text-white h-8 w-8 mr-2"
-          >
-            <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
-          </svg>
-          <span className="text-xl text-white font-bold uppercase tracking-wide">
-            Talwind CSS
-          </span>
+          <h1 className="text-xl font-bold uppercase tracking-wide">
+            unbounded
+          </h1>
         </Link>
         <button
-          className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
+          className=" inline-flex p-3 hover:bg-orange-600 rounded lg:hidden ml-auto hover:text-white outline-none"
           onClick={handleClick}
         >
           <svg
@@ -49,32 +42,61 @@ export const Navbar = () => {
             active ? "" : "hidden"
           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
         >
-          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+          <div className=" transition-all lg:inline-flex lg:flex-row lg:ml-auto lg:mr-auto lg:w-auto w-full lg:items-center items-end  flex flex-col lg:h-auto">
+            <div className="lg:hidden w-full flex flex-col justify-end items-end justify-items-end">
+              <Link
+                href="/"
+                className="text-center my-1 max-w-[120px] transition-all lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center shadow-md shad text-white bg-orange-600"
+              >
+                <h6 className="text-center  shadow-black">Sign Up</h6>
+              </Link>
+              <Link
+                href="/"
+                className="text-center  my-1 max-w-[120px] transition-all lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center shadow-md bg-gray-300"
+              >
+                <h6 className="text-center  shadow-black">Log In</h6>
+              </Link>
+            </div>
             <Link
               href="/"
-              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white "
+              className="max-w-[120px] text-right transition-all lg:inline-flex lg:w-auto w-full px-3 py font-bold items-center justify-center border-b-4 border-b-transparent hover:border-b-orange-600"
             >
               Home
             </Link>
             <Link
               href="/"
-              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white"
+              className="max-w-[120px] text-right transition-all lg:inline-flex lg:w-auto w-full px-3 py font-bold items-center justify-center border-b-4 border-b-transparent hover:border-b-orange-600"
             >
               Services
             </Link>
             <Link
               href="/"
-              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white"
+              className="max-w-[120px] text-right transition-all lg:inline-flex lg:w-auto w-full px-3 py font-bold items-center justify-center border-b-4 border-b-transparent hover:border-b-orange-600"
             >
               About us
             </Link>
             <Link
               href="/"
-              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white"
+              className="max-w-[120px] text-right transition-all lg:inline-flex lg:w-auto w-full px-3 py font-bold items-center justify-center border-b-4 border-b-transparent hover:border-b-orange-600"
             >
               Contact us
             </Link>
           </div>
+        </div>
+        {/* login and sign up buttons */}
+        <div className="text-center  hidden lg:inline-flex ">
+          <Link
+            href="/"
+            className="text-center transition-all lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center shadow-md mx-auto mr-3 bg-gray-300"
+          >
+            <h6 className="text-center  shadow-black">Log In</h6>
+          </Link>
+          <Link
+            href="/"
+            className="text-center  transition-all lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center shadow-md mx-auto mr-3 shad text-white bg-orange-600"
+          >
+            <h6 className="text-center  shadow-black">Sign Up</h6>
+          </Link>
         </div>
       </nav>
     </>
